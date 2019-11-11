d7cce81c07191552a70687d25c48d6070eada038
github username <github email address>
2019-11-11 15:26:45 -0500

    all

--- Frontend/index.js
+++ Frontend/index.js
@@ -139,7 +139,6 @@ function deleteReview(event){
   button.parentElement.remove()
 }
 
-
 function submitReview(event){
   event.preventDefault()
   fetch(`http://localhost:3000/reviews/`, {
@@ -177,4 +176,82 @@ function submitReview(event){
 
 }
 
+function addNewProduct(respond){
+
+  newProductBTN.addEventListener("click", () => {
+    let newProductFORM = document.createElement("form")
+    // name
+    let nameLabel = document.createElement("label")
+    nameLabel.innerText = "Name:"
+    let nameInput = document.createElement("input")
+    nameInput.name = "name"
+    nameInput.type = "text"
+    // image
+    let imageLabel = document.createElement("label")
+    imageLabel.innerText = "Image:"
+    let imageInput = document.createElement("input")
+    imageInput.name = "image"
+    imageInput.type = "text"
+    // description
+    let descLabel = document.createElement("label")
+    descLabel.innerText = "Description:"
+    let descInput = document.createElement("input")
+    descInput.name = "description"
+    descInput.type = "text"
+    // origin
+    let originLabel = document.createElement("label")
+    originLabel.innerText = "Origin:"
+    let originInput = document.createElement("input")
+    originInput.name = "origin"
+    originInput.type = "text"
+    // cost
+    let costLabel = document.createElement("label")
+    costLabel.innerText = "Cost:"
+    let costInput = document.createElement("input")
+    costInput.name = "cost"
+    costInput.type = "number"
+    // category
+    let categoryLabel = document.createElement("label")
+    categoryLabel.innerText = "Category:"
+    let categorySelect = document.createElement("select")
+    categorySelect.name = "selectCategory"
+    respond.forEach((category) => {
+      let categoryOption = document.createElement("option")
+      categoryOption.innerText = category.name
+      categoryOption.id = category.id
+      categorySelect.append(categoryOption)
+    })
+    // submit
+    let submitInput = document.createElement("input")
+    submitInput.value = "ADD TO THE BLACK MARKET"
+    submitInput.type = "submit"
+
+    newProductBTN.remove()
+
+    newProductFORM.append(nameLabel, nameInput, imageLabel, imageInput, descLabel, descInput, originLabel, originInput, costLabel, costInput, categoryLabel, categorySelect, submitInput)
+    addProductDIV.append(newProductFORM)
+
+    newProductFORM.addEventListener("submit", (e) => {
+      debugger
+      e.preventDefault()
+      fetch(`http://localhost:3000/products`, {
+        method:'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: e.target.name.value,
+          image: e.target.image.value,
+          description: e.target.description.value,
+          origin: e.target.origin.value,
+          cost: e.target.cost.value,
+          category_id: e.target.selectCategory.selectedIndex + 1
+
+
+        })
+      })
+      debugger
+    })
+ })
+}
 // debugger
