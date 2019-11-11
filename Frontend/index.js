6c621a244da584fab081d038a2687a34ac2a07de
github username <github email address>
2019-11-11 15:23:47 -0500

    delete review

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,23 +1,18 @@
 let categoriesUl = document.querySelector('#categories')
 let productsUl = document.querySelector('#products')
 let productDiv = document.querySelector('#display-product')
-let newProductBTN = document.getElementById("new-product")
-let addProductDIV = document.getElementById("add-product")
 
 fetch('http://localhost:3000/categories')
 .then(r => r.json())
 .then(respond => {
-  
   respond.forEach(item => {
     let li = document.createElement('LI')
     li.innerText = item.name
     li.setAttribute('data-id' , item.id)
     li.addEventListener('click', event => {renderProductsList(event)})
-    categoriesUl.append(li)   
+    categoriesUl.append(li)
   })
-  addNewProduct(respond)
 })
-
 
 function renderProductsList(event){
   fetch(`http://localhost:3000/categories/${event.target.dataset.id}`)
@@ -136,82 +131,50 @@ function renderProduct(event){
   })
 }
 
-function addNewProduct(respond){
-  
-  newProductBTN.addEventListener("click", () => {
-    let newProductFORM = document.createElement("form")
-    // name
-    let nameLabel = document.createElement("label")
-    nameLabel.innerText = "Name:"
-    let nameInput = document.createElement("input")
-    nameInput.name = "name"
-    nameInput.type = "text"
-    // image
-    let imageLabel = document.createElement("label")
-    imageLabel.innerText = "Image:"
-    let imageInput = document.createElement("input")
-    imageInput.name = "image"
-    imageInput.type = "text"
-    // description
-    let descLabel = document.createElement("label")
-    descLabel.innerText = "Description:"
-    let descInput = document.createElement("input")
-    descInput.name = "description"
-    descInput.type = "text"
-    // origin
-    let originLabel = document.createElement("label")
-    originLabel.innerText = "Origin:"
-    let originInput = document.createElement("input")
-    originInput.name = "origin"
-    originInput.type = "text"
-    // cost
-    let costLabel = document.createElement("label")
-    costLabel.innerText = "Cost:"
-    let costInput = document.createElement("input")
-    costInput.name = "cost"
-    costInput.type = "number"
-    // category
-    let categoryLabel = document.createElement("label")
-    categoryLabel.innerText = "Category:"
-    let categorySelect = document.createElement("select")
-    categorySelect.name = "selectCategory"
-    respond.forEach((category) => {
-      let categoryOption = document.createElement("option")
-      categoryOption.innerText = category.name
-      categoryOption.id = category.id 
-      categorySelect.append(categoryOption)
-    })
-    // submit
-    let submitInput = document.createElement("input")
-    submitInput.value = "ADD TO THE BLACK MARKET"
-    submitInput.type = "submit"
-
-    newProductBTN.remove()
-
-    newProductFORM.append(nameLabel, nameInput, imageLabel, imageInput, descLabel, descInput, originLabel, originInput, costLabel, costInput, categoryLabel, categorySelect, submitInput)
-    addProductDIV.append(newProductFORM)
-
-    newProductFORM.addEventListener("submit", (e) => {
-      debugger
-      e.preventDefault()
-      fetch(`http://localhost:3000/products`, {
-        method:'POST',
-        headers: { 
-          'Content-type': 'application/json'
-        },
-        body: JSON.stringify({
-          name: e.target.name.value,
-          image: e.target.image.value,
-          description: e.target.description.value,
-          origin: e.target.origin.value,
-          cost: e.target.cost.value,
-          category_id: e.target.selectCategory.selectedIndex + 1
-
-          
-        })
-      })
-      debugger
-    })
- })
+function deleteReview(event){
+  fetch(`http://localhost:3000/reviews/${event.target.id}`, {
+    method: 'DELETE'
+  })
+  let button = document.getElementById(event.target.id)
+  button.parentElement.remove()
 }
+
+
+function submitReview(event){
+  event.preventDefault()
+  fetch(`http://localhost:3000/reviews/`, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify({
+      author: event.target['author'].value,
+      text: event.target['review'].value,
+      rating: event.target['rating'].value,
+      product_id: event.target.id
+    })
+  })
+  .then(r => r.json())
+  .then(respond => {
+    let productDiv = document.querySelector('#display-product')
+    let reviewsUl = productDiv.querySelector('#reviews-list')
+    let newReviewLi = document.createElement('li')
+    let newReviewText = document.createElement('p')
+    let newReviewAuthor = document.createElement('p')
+    let deleteButton = document.createElement('dutton')
+
+    newReviewText.innerText = respond.text
+    newReviewAuthor.innerText = respond.author
+
+    deleteButton.innerText = 'X'
+    deleteButton.setAttribute('id', respond.id)
+    deleteButton.addEventListener('click', event => {deleteReview(event)})
+
+    newReviewLi.append(newReviewText, newReviewAuthor, deleteButton)
+    reviewsUl.append(newReviewLi)
+    productDiv.append(reviewsUl)
+  })
+
+}
+
 // debugger
